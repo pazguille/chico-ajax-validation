f956e785897aa65ae88b8671149eee616d390f78
Guille Paz <guille87paz@gmail.com>
2014-01-15 11:52:49 -0300

    Add som comments to sync version.

--- src/sync.validation.js
+++ src/sync.validation.js
@@ -1,23 +1,30 @@
 (function (window){
-    // SYNC
-    var sync = $('#zip_sync').required().and().custom('sync', function (value) {
-        var ok;
 
-        //ZIP CODE: 90040060
-        $.ajax({
-            'url': 'https://api.mercadolibre.com/countries/BR/zip_codes/' + value,
-            'dataType': 'json',
-            'type': 'GET',
-            'async': false
-        })
-        .done(function () {
-            ok = true;
-        })
-        .error(function () {
-            ok = false;
-        });
+    // Creates a required validation
+    var sync = $('#zip_sync')
+        .required()
+        .and()
 
-        return ok;
+        // Creates a custom validation wich will make an ajax call syncr.
+        .custom('sync', function (value) {
 
-    }, 'Message Sync.');
+            // This flag will be returned by custom validation after ajax call finishes.
+            var ok;
+
+            $.ajax({
+                'url': 'https://api.mercadolibre.com/countries/BR/zip_codes/' + value,
+                'dataType': 'json',
+                'type': 'GET',
+                'async': false
+            })
+            .done(function () {
+                ok = true;
+            })
+            .error(function () {
+                ok = false;
+            });
+
+            return ok;
+
+        }, 'Please, enter a valid zip code.');
 }(this));
